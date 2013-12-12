0622ce8ba663ce09c3c134ea66ffbfb2da6e63da
Alexander Matrunich <alexander@matrunich.com>
2013-12-12 15:16:54 +0100

    SQL request of item types from DB admin

--- sandbox.r
+++ sandbox.r
@@ -27,9 +27,28 @@ where area.area = 33 and area.area = TS_ICS_WORK_YR.area
 and item.item = TS_ICS_WORK_YR.item")
 
 y <- sws_query(class.path="../sws/ojdbc14.jar", dbquery=
-"select * from FAOSTAT.TS_ICS_WORK_YR
-where area = 33 and item=15
+"SELECT         FAOSTAT.ITEM.ITEM, FAOSTAT.ITEM.NAME_E AS ItemName, FAOSTAT.ITEM_TYP_ELE_DISPLAY_ELE.ELE, 
+                         FAOSTAT.ITEM_TYP_ELE_DISPLAY_ELE.DISPLAY_ELE, FAOSTAT.ITEM_TYP_ELE_DISPLAY_ELE.STD_UNIT, 
+                         FAOSTAT.ITEM_TYP_ELE_DISPLAY_ELE.LONG_NAME_E AS EleName
+FROM            FAOSTAT.ITEM INNER JOIN
+                         FAOSTAT.ITEM_TYP_ELE_DISPLAY_ELE ON FAOSTAT.ITEM.ITEM_TYP = FAOSTAT.ITEM_TYP_ELE_DISPLAY_ELE.ITEM_TYP
 "
 )
 
-y1 <- sws_query(ele=c(31, 51), item=15, year=1960:1970, class.path="../sws/ojdbc14.jar")
+y1 <- sws_query(ele=c(31, 51), item=15, year=1960:1980, class.path="../sws/ojdbc14.jar")
+library(stringr)
+library(reshape2)
+
+y1l <- melt(y1, measure.vars=names(y1)[str_detect(names(y1), '(^num_)|(^symb_)')])
+
+
+str_extract(names(y1), '([0-9]{2}$)')
+
+x <- names(y1)[5]
+orignumb <- as.numeric(str_extract(x, '([0-9]{2}$)'))
+corryear <- orignumb + 1959
+corrname <- str_c(str_replace(x, '(^.*)([0-9]{2}$)', '\\1'), corryear)
+corrname
+                  
+str_detect(c('area', 'num_01', '!symb_01'), perl('(!symb)'))
+
